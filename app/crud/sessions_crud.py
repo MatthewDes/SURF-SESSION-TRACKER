c575da00c626b7a617ffeca57219327778c0cfef
MatthewDes <matthewmazedesmet@icloud.com>
2025-10-11 12:23:19 +0000

    create session PUT crud function

--- app/crud/sessions_crud.py
+++ app/crud/sessions_crud.py
@@ -16,3 +16,19 @@ def get_session(db: Session, session_id: int):
 
 def get_all_sessions(db: Session, skip: int = 0, limit: int = 100):
     return db.query(db_models.Session).offset(skip).limit(limit).all()
+
+
+#PUT
+def update_session(db: Session, session_id: int, updated_session: schemas.SessionCreate):
+    db_session = db.query(db_models.Session).filter(db_models.Session.id == session_id).first() 
+    if db_session is None:
+        return None
+
+    update_data = updated_session.model_dump()
+
+    for key, value in update_data.items():
+        setattr(db_session, key, value)
+
+    db.commit()
+    db.refresh(db_session)
+    return db_session
